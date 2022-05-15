a1f89517cbc4fdbdbcb5343ff39df9e63dec696e
jerumanu <emmanueljeru200@gmail.com>
2022-05-15 18:18:26 +0300

    innitial commit

---
+++ src/components/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import   Search from "./Search.js";
+// import Accordion from './Accordion';
+
+// const items= [
+//   {title: 'What is React',
+//   content:'React is a fronett end JavaScript frameworks'
+
+   
+// },
+// {
+//   title:'why use react?',
+//   content:'React is a favorite JS library among eng '
+
+// },
+// {
+//   title: 'How do you use React',
+//   content:'You use React by creating components'
+// }
+// ]
+// eslint-disable-next-line import/no-anonymous-default-export
+export default () => {
+  return(
+    <div>
+    {/* <Accordion  items ={items} /> */}
+    <Search />
+    
+    </div>
+  )
+};
