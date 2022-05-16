a92bc6d76e1a5a02ed8fc05459e8de209028f659
jerumanu <emmanueljeru200@gmail.com>
2022-05-16 21:41:12 +0300

    clean  useeffect fuctions

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -5,15 +5,23 @@ import React, { useState,useEffect ,useRef} from "react";
 const  Dropdown = ({options,selected,onSelectedChange})=>{
     const[open,setOpen]= useState(false);
     const ref = useRef();
+    
+    
 
     useEffect(()=>{
-        document.body.addEventListener('click',(event)=>{
+        const onBodyClick=(event)=>{
             if (ref.current.contains(event.target)){
                 return;
             }
             setOpen(false);
+        }
 
-        })
+        document.body.addEventListener('click',onBodyClick);
+            return()=>{
+                document.body.removeEventListener('click',onBodyClick)
+            };
+
+       
     },[]);
 const renderedOptions = options.map((option)=> {
     if (option.value ===selected.value){
