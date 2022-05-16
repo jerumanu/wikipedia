8f3da4872bdbe83c842e4aa39b1b72ff6be1ef1f
jerumanu <emmanueljeru200@gmail.com>
2022-05-17 00:27:01 +0300

    delited arrays

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React ,{useState} from 'react';
 import Dropdown from './Dropdown';
+import Translate from './Translate';
 // import   Search from "./Search.js";
 // import Accordion from './Accordion';
 
@@ -35,15 +36,16 @@ const options= [
 ];
 // eslint-disable-next-line import/no-anonymous-default-export
 export default () => {
-  const [selected ,setSelected]= useState(options[0]);
-  const[showDropdown, setShowDropdown] = useState(true);
+  // const [selected ,setSelected]= useState(options[0]);
+  // const[showDropdown, setShowDropdown] = useState(true);
   return(
     <div>
+      <Translate />
     {/* <Accordion  items ={items} /> */}
     {/* <Search /> */}
-    <button onClick={()=> setShowDropdown(!showDropdown)}>Toggle Dropdown</button>
-    { showDropdown ?
-      <Dropdown selected={selected} onSelectedChange={setSelected} options={options} /> :null} 
+    {/* <button onClick={()=> setShowDropdown(!showDropdown)}>Toggle Dropdown</button> */}
+    {/* { showDropdown ? */}
+      {/* <Dropdown selected={selected} onSelectedChange={setSelected} options={options} /> :null}  */}
     </div>
   )
 };
