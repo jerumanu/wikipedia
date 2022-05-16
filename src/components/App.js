74670e06854986e9d7a074fe480b4fcae53f134e
jerumanu <emmanueljeru200@gmail.com>
2022-05-16 07:48:08 +0300

    imported dropdown componebts and add array of options

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import   Search from "./Search.js";
+import React ,{useState} from 'react';
+import Dropdown from './Dropdown';
+// import   Search from "./Search.js";
 // import Accordion from './Accordion';
 
 // const items= [
@@ -18,12 +19,28 @@ import   Search from "./Search.js";
 //   content:'You use React by creating components'
 // }
 // ]
+const options= [
+{
+  label:'the Color Red',
+  value: 'red'
+},
+{
+  label:'The Color Green',
+  value:'green'
+},
+{
+  label:'A Shade of Blue',
+  value:'blue'
+}
+];
 // eslint-disable-next-line import/no-anonymous-default-export
 export default () => {
+  const [selected ,setSelected]= useState(options[0]);
   return(
     <div>
     {/* <Accordion  items ={items} /> */}
-    <Search />
+    {/* <Search /> */}
+    <Dropdown selected={selected} onSelectedChange={setSelected} options={options} />
     
     </div>
   )
