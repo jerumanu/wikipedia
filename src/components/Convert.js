8c2e3514eefbdee87a3f925e25d171ccf3611c2a
jerumanu <emmanueljeru200@gmail.com>
2022-05-17 09:09:18 +0300

    make request

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -1,11 +1,31 @@
-import React,{useState,UseEffect} from 'react'
+import React,{useState, useEffect} from 'react'
+import axios from 'axios'
 
 
-const Convert =()=>{
+
+const Convert =({language,text})=>{
+    const [translated,setTranslated]=useState('');
+
+    useEffect (()=>{
+
+        const doTransaltion= async ()=>{
+            const{data} =await axios.post('https://translation.googleapis.com/language/v2',{},{
+            params:{
+                q:text,
+                target:language.value,
+                key:'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM'
+            }
+            
+        })
+        setTranslated(data.data.translations[0].translatedText);
+    }
+    doTransaltion();
+
+    } ,[language,text])
 
     return(
         <div>
-
+            <h1 className='ui header'> {translated}</h1>
         </div>
     )
 }
