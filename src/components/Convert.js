2905238bd60bf351a2d6939e6673efd45d7bd1d3
jerumanu <emmanueljeru200@gmail.com>
2022-05-17 09:26:02 +0300

    set a timeout

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -5,13 +5,22 @@ import axios from 'axios'
 
 const Convert =({language,text})=>{
     const [translated,setTranslated]=useState('');
+    const [debouncedText ,setDebouncedText] = useState('text');
+    useEffect(()=>{
+        const timerId = setTimeout(() => {
+            setDebouncedText(text);
+        }, 500);
+        return()=>{
+            clearTimeout(timerId);
+        }
+    }, [text]);
 
     useEffect (()=>{
 
         const doTransaltion= async ()=>{
             const{data} =await axios.post('https://translation.googleapis.com/language/v2',{},{
             params:{
-                q:text,
+                q:debouncedText,
                 target:language.value,
                 key:'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM'
             }
@@ -21,7 +30,7 @@ const Convert =({language,text})=>{
     }
     doTransaltion();
 
-    } ,[language,text])
+    } ,[language,debouncedText])
 
     return(
         <div>
